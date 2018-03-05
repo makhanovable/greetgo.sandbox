4c5d26f4292dfd130d567ee7382a5be00d727d0e
damze <damze@greet-go.com>
2018-03-05 17:26:12 +0600

    models for tmp tables

---
+++ sandbox.db/src/kz/greetgo/sandbox/db/register_impl/migration/model/AddressCia.java
@@ -0,0 +1,15 @@
+package kz.greetgo.sandbox.db.register_impl.migration.model;
+
+
+import kz.greetgo.sandbox.controller.enums.AddressType;
+
+public class AddressCia {
+  public String client;
+  public String cia_id;
+  public AddressType type;
+  public String street;
+  public String house;
+  public String flat;
+  public String error;
+
+}
---
+++ sandbox.db/src/kz/greetgo/sandbox/db/register_impl/migration/model/PhoneCia.java
@@ -0,0 +1,10 @@
+package kz.greetgo.sandbox.db.register_impl.migration.model;
+
+import kz.greetgo.sandbox.controller.enums.PhoneNumberType;
+
+public class PhoneCia {
+  public String client;
+  public String cia_id;
+  public String number;
+  public PhoneNumberType type;
+}
