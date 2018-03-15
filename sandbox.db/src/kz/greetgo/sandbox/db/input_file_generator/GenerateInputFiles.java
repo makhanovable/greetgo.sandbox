5e438aade1b87696453b82296dca73acfc7c19c1
damze <damze@greet-go.com>
2018-03-15 17:00:14 +0600

    limit changed

--- sandbox.db/src/kz/greetgo/sandbox/db/input_file_generator/GenerateInputFiles.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/input_file_generator/GenerateInputFiles.java
@@ -27,8 +27,8 @@ import java.util.stream.Collectors;
 
 public class GenerateInputFiles {
 
-  public static final int CIA_LIMIT = 1_000;
-  public static final int FRS_LIMIT = 100_000;
+  public static final int CIA_LIMIT = 1_000_000;
+  public static final int FRS_LIMIT = 10_000_000;
 
   public static void main(String[] args) throws Exception {
     new GenerateInputFiles().execute();
