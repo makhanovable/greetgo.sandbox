98c68be11d882624b6a3195b5505c003c0e0613f
damze <damze@greet-go.com>
2018-03-15 19:06:45 +0600

    date format now more detailed

--- sandbox.db/src/kz/greetgo/sandbox/db/util/DateUtils.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/util/DateUtils.java
@@ -18,7 +18,7 @@ public class DateUtils {
     long diffHours = diff / (60 * 60 * 1000) % 24;
     long diffDays = diff / (24 * 60 * 60 * 1000);
 
-    return String.format("%d days %d_%d_%d_%d", diffDays, diffHours, diffMinutes, diffSeconds, diffMiliSeconds);
+    return String.format("%d days %d hours %d minutes %d seconds %d milisec", diffDays, diffHours, diffMinutes, diffSeconds, diffMiliSeconds);
   }
 
 }
