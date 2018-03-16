415938dea55e280d840216dc2a9acff7ab01793e
damze <damze@greet-go.com>
2018-03-16 19:18:41 +0600

    mig statuses

---
+++ sandbox.db/src/kz/greetgo/sandbox/db/register_impl/migration/MigrationStatuses.java
@@ -0,0 +1,11 @@
+package kz.greetgo.sandbox.db.register_impl.migration;
+
+public class MigrationStatuses {
+
+  public static int INVALID = 0;
+  public static int NOT_READY = 1;
+  public static int LAST_ACTUAL = 4;
+  public static int TO_INSERT = 2;
+  public static int TO_UPDATE = 3;
+
+}
