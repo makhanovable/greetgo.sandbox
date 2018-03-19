dea4866076427e36353931a50d817ce045cbdde2
damze <damze@greet-go.com>
2018-03-19 09:42:25 +0600

    migration status

--- sandbox.db/src/kz/greetgo/sandbox/db/register_impl/migration/MigrationStatus.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/register_impl/migration/MigrationStatus.java
@@ -1,11 +1,11 @@
 package kz.greetgo.sandbox.db.register_impl.migration;
 
-public class MigrationStatuses {
+@SuppressWarnings("WeakerAccess")
+public class MigrationStatus {
 
-  public static int INVALID = 0;
   public static int NOT_READY = 1;
-  public static int LAST_ACTUAL = 4;
   public static int TO_INSERT = 2;
   public static int TO_UPDATE = 3;
+  public static int LAST_ACTUAL = 4;
 
 }
