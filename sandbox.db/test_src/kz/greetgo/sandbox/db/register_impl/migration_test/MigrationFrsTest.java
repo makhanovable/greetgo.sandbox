1d6d039c6565a0c81b18234a08548921e1586096
damze <damze@greet-go.com>
2018-03-19 19:47:15 +0600

    frs migration for testing wrapper

---
+++ sandbox.db/test_src/kz/greetgo/sandbox/db/register_impl/migration_test/MigrationFrsTest.java
@@ -0,0 +1,34 @@
+package kz.greetgo.sandbox.db.register_impl.migration_test;
+
+import kz.greetgo.sandbox.db.register_impl.migration.MigrationConfig;
+import kz.greetgo.sandbox.db.register_impl.migration.MigrationFrs;
+import kz.greetgo.sandbox.db.register_impl.migration.enums.TmpTableName;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class MigrationFrsTest extends MigrationFrs {
+  public MigrationFrsTest(MigrationConfig config, Connection connection) {
+    super(config, connection);
+  }
+
+
+  @Override
+  public void migrate() {
+    //do nothing
+  }
+
+  public void ParseAndInsertIntoTempTables() throws Exception {
+    super.parseFileAndUploadToTempTables();
+  }
+
+  public void createTempTables() throws SQLException {
+    super.createTempTables();
+  }
+
+
+  public Map<TmpTableName, String> getTableNames() {
+    return tableNames;
+  }
+}
