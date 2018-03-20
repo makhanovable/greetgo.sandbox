3c50eadd43e32adc0d78b2481b64b770d6bae272
greetgo! <ekolpakov@greet-go.com>
2018-03-20 17:58:05 +0600

    Update Account.java

--- sandbox.stand/db/src/kz/greetgo/sandbox/db/stand/model/Account.java
+++ sandbox.stand/db/src/kz/greetgo/sandbox/db/stand/model/Account.java
@@ -10,17 +10,13 @@ public class Account {
     public String number;
     public Timestamp registered_at;
 
+    // TODO: удалить, если не используешь
     public float getMinCash(Map<String, Transaction> transactions) {
         float minCash = this.money;
         float cash = 0;
         for (Transaction tr : transactions.values()) {
-            //TODO: неверное выражение "tr.accountID.equals(this.id)". 
-            // Подумай почему, если не получится узнать, позже спроси у меня.
             if (Objects.equals(tr.accountID, this.id)) {
                 cash += tr.money;
-                //TODO: ты неверно понял схему БД. Разберись, я потом спрошу.
-                // Здесь найдена не тот минимум
-                // (Done) Перенес операцию в ClientRegisterStand
                 if (cash < minCash) minCash = cash;
             }
         }
@@ -28,14 +24,13 @@ public class Account {
         return minCash;
     }
 
+    // TODO: удалить, если не используешь
     public float getMaxCash(Map<String, Transaction> transactions) {
         float maxCash = 0;
         float cash = 0;
         for (Transaction tr : transactions.values()) {
-            //TODO: аналогично. Исправить.
             if (tr.accountID.equals(this.id)) {
                 cash += tr.money;
-                //TODO: аналогично. Разобраться в БД.
                 if (cash > maxCash) maxCash = cash;
             }
         }
