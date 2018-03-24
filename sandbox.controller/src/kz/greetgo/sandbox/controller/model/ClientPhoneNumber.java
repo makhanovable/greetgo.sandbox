672ef292f3323acab97f27b5f62daeaa9fa7f87d
damze <damze@greet-go.com>
2018-03-24 16:21:23 +0600

    equals hashcode generated

--- sandbox.controller/src/kz/greetgo/sandbox/controller/model/ClientPhoneNumber.java
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/model/ClientPhoneNumber.java
@@ -6,4 +6,23 @@ public class ClientPhoneNumber {
   public String client;
   public String number;
   public PhoneNumberType type;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ClientPhoneNumber)) return false;
+
+    ClientPhoneNumber that = (ClientPhoneNumber) o;
+
+    //noinspection SimplifiableIfStatement
+    if (number != null ? !number.equals(that.number) : that.number != null) return false;
+    return type == that.type;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = number != null ? number.hashCode() : 0;
+    result = 31 * result + (type != null ? type.hashCode() : 0);
+    return result;
+  }
 }
