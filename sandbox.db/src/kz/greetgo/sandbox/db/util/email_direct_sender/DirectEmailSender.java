949b184ab2a54c8fd52462cfb103d9be124ef819
ekolpakov <ekolpakov@greet-go.com>
2018-04-18 12:56:15 +0600

    Вынесены аккаунты в файл

--- sandbox.db/src/kz/greetgo/sandbox/db/util/email_direct_sender/DirectEmailSender.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/util/email_direct_sender/DirectEmailSender.java
@@ -1,5 +1,7 @@
 package kz.greetgo.sandbox.db.util.email_direct_sender;
 
+import kz.greetgo.conf.ConfData;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -10,7 +12,21 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class DirectEmailSender {
+
   public static void main(String[] args) {
+    new DirectEmailSender().exec();
+  }
+
+  public void exec() {
+    ConfData c = new ConfData();
+    c.readFromStream(getClass().getResourceAsStream("account_info.txt"));
+
+    String emailAccountName = c.str("by_mail");
+    String emailAccountPassword = c.str("by_pass");
+    String toEmail1 = c.str("email1");
+    String toEmail2 = c.str("email2");
+    String toEmail3 = c.str("email3");
+
     Properties properties = new Properties();
 
     properties.setProperty("mail.smtp.host", "smtp.gmail.com");
@@ -22,16 +38,16 @@ public class DirectEmailSender {
     Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
       @Override
       protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication("jkolpakov@gmail.com", "jkolpakov123");
+        return new PasswordAuthentication(emailAccountName, emailAccountPassword);
       }
     });
 
     try {
       MimeMessage message = new MimeMessage(session);
 
-      message.addRecipient(Message.RecipientType.TO, new InternetAddress("ekolpakov@greet-go.com"));
-      message.addRecipient(Message.RecipientType.CC, new InternetAddress("john.kolpakov.x@gmail.com"));
-      message.addRecipient(Message.RecipientType.BCC, new InternetAddress("pompei@mail.ru"));
+      message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail1));
+      message.addRecipient(Message.RecipientType.CC, new InternetAddress(toEmail2));
+      message.addRecipient(Message.RecipientType.BCC, new InternetAddress(toEmail3));
 
       message.setSubject("Привет от прекрасного далёка");
 
@@ -42,6 +58,14 @@ public class DirectEmailSender {
           " Привет всем. <i>Это текст курсивом</i>"
 
         , "text/html; charset=UTF-8");
+
+      message.setText(
+
+        "This is красное " +
+          "сообщение. Hi all." +
+          " Привет всем. Это текст курсивом"
+
+        , "UTF-8");
 
       Transport.send(message);
       System.out.println("Sent message successfully....");
