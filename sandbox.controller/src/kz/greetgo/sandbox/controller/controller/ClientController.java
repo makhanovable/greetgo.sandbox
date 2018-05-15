641e15cef727cdfc8c5f4d97d3dacfb6dd18fbc6
greetgo! <ekolpakov@greet-go.com>
2018-05-15 14:27:56 +0600

    Update ClientController.java

--- sandbox.controller/src/kz/greetgo/sandbox/controller/controller/ClientController.java
+++ sandbox.controller/src/kz/greetgo/sandbox/controller/controller/ClientController.java
@@ -31,7 +31,6 @@ public class ClientController implements Controller {
 
     @ToJson
     @Mapping("/clientsInfo/{pageID}/{filterStr}/{sortBy}/{sortOrder}")
-    //TODO: 2.01. Имена мапингов контроллеров должны совпадать с именами методов контроллеров.
     public ClientToReturn getFilteredClients(@ParPath("pageID") String pageID, @ParPath("filterStr") String filterStr,
             @ParPath("sortBy") String sortBy, @ParPath("sortOrder") String sortOrder) {
 
