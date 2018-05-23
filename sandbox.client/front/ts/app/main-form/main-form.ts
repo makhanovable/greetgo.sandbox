0c75400eec797d0b97707e3fb48e7263f3ab35e8
NKabylbay <nkabylbai@gmail.com>
2018-05-23 10:58:26 +0600

    Refactoring in main form component

--- sandbox.client/front/ts/app/main-form/main-form.ts
+++ sandbox.client/front/ts/app/main-form/main-form.ts
@@ -6,6 +6,7 @@ import {Subscription} from "rxjs/Subscription";
 import {MatDialog, MatDialogConfig} from "@angular/material";
 import {ModalInfoComponent} from "./components/modal-info/modal-info";
 import {ActionType} from "../../model/ActionType";
+import {Constants} from "../../Constants";
 
 @Component({
   selector: 'main-form-component',
@@ -22,20 +23,18 @@ export class MainFormComponent implements OnDestroy {
   loadUserInfoButtonEnabled: boolean = true;
   loadUserInfoError: string | null;
 
-  isEditMode = false;
-  DUMB_ID = -1;
-
-  constructor(private httpService: HttpService, private dialog: MatDialog) {}
+  constructor(private httpService: HttpService, private dialog: MatDialog) {
+  }
 
   handleCreateAccClick = function () {
-    this.openModal(this.DUMB_ID, ActionType.CREATE);
+    this.openModal(Constants.DUMB_ID, ActionType.CREATE);
   };
 
   handleEditAccClick = function (accountInfo) {
     this.openModal(accountInfo.id, ActionType.EDIT);
   };
 
-  openModal(clientId:number, actionType: ActionType) {
+  openModal(clientId: number, actionType: ActionType) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
@@ -52,16 +51,28 @@ export class MainFormComponent implements OnDestroy {
     this.loadUserInfoButtonEnabled = false;
     this.loadUserInfoError = null;
 
-    this.httpService.get("/auth/userInfo").toPromise().then(result => {
-      this.userInfo = UserInfo.copy(result.json());
-      let phoneType: PhoneType | null = this.userInfo.phoneType;
-      console.log(phoneType);
+    this.requestUserInfo();
+  }
+
+  private requestUserInfo() {
+    this.httpService.get("/auth/userInfo").toPromise().then(response => {
+      this.onUserInfoRequestSuccess(response);
     }, error => {
-      console.log(error);
-      this.loadUserInfoButtonEnabled = true;
-      this.loadUserInfoError = error;
-      this.userInfo = null;
+      this.onUserInfoRequestError(error);
     });
+  }
+
+  private onUserInfoRequestSuccess(response) {
+    this.userInfo = UserInfo.copy(response.json());
+    let phoneType: PhoneType | null = this.userInfo.phoneType;
+    console.log(phoneType);
+  }
+
+  private onUserInfoRequestError(error) {
+    console.log(error);
+    this.loadUserInfoButtonEnabled = true;
+    this.loadUserInfoError = error;
+    this.userInfo = null;
   }
 
   ngOnDestroy(): void {
